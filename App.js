dfe0d13dc5c2c3ea880e54d4babccada7ea6c491
Dacre <daniele.dominici18@gmail.com>
2021-05-30 22:12:43 +0200

    added buttons

--- App.js
+++ App.js
@@ -1,22 +1,29 @@
 import React, { useState } from "react";
-import { StyleSheet } from "react-native";
+import { Linking, StyleSheet, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import FlatListGridCard from "./app/screens/FlatListGridCard";
 import TabNavigationInfo from "./app/screens/RecyclerViewScreens/TabNavigationInfo";
+import { FAB } from "react-native-paper";
+import DefColors from "./DefColors";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   const [charName, setCharName] = useState("ciao");
+
   return (
     <NavigationContainer
       style={styles.container}
       linking={{ enabled: true }}
       theme={MyTheme}
     >
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerRight: () => <FABpersonal />,
+        }}
+      >
         <Stack.Screen name="Home">
           {(props) => (
             <FlatListGridCard
@@ -34,13 +41,56 @@ export default function App() {
   );
 }
 
+function FABpersonal() {
+  return (
+    <View style={styles.viewContainer}>
+      <FAB
+        onPress={() => {
+          Linking.openURL("https://twitter.com/Dacrenzio1");
+        }}
+        title="help"
+        icon="twitter"
+        style={styles.fabs}
+        color="#00A0FF"
+      />
+      <FAB
+        onPress={() => {
+          Linking.openURL("https://discord.gg/dWnPTmnbXM");
+        }}
+        title="Discord"
+        icon="discord"
+        style={styles.fabs}
+        color="#5865f2"
+      />
+      <FAB
+        onPress={() => {
+          Linking.openURL("https://t.co/pNPacF4dWb");
+        }}
+        title="Resource File"
+        icon="attachment"
+        style={styles.fabs}
+        color="#000"
+      />
+      <FAB
+        onPress={() => {
+          Linking.openURL("https://t.co/pNPacF4dWb");
+        }}
+        title="Donation"
+        icon={{ uri: "https://cdnlogo.com/logos/p/42/paypal-icon.svg" }}
+        style={styles.fabs}
+        color="#00A0FF"
+      />
+    </View>
+  );
+}
+
 const MyTheme = {
   dark: false,
   colors: {
     primary: "#5efcff",
     background: "#222222",
-    card: "#5000FF",
-    text: "rgb(255, 255, 255)",
+    card: "#8f208f",
+    text: "#fff",
     border: "rgb(0, 0, 0)",
   },
 };
@@ -48,5 +98,18 @@ const MyTheme = {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignContent: "center",
+  },
+  fabs: {
+    justifyContent: "center",
+    borderRadius: 15,
+    margin: 6,
+    backgroundColor: DefColors.primaryRow,
+  },
+  viewContainer: {
+    flexDirection: "row",
+    flex: 1,
+    justifyContent: "center",
   },
 });
